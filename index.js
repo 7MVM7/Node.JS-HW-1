42491f8d7bbf94c1aea77e0e4997f02833d66ce8
Vladyslaw Makhnyk <itdelive@gmail.com>
2021-12-13 22:04:45 +0200

    Complete

---
+++ index.js
@@ -0,0 +1,38 @@
+const contacts = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", " id")
+  .option("-n, --name <type>", " name")
+  .option("-e, --email <type>", "email")
+  .option("-p, --phone <type>", " phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      contacts.listContacts();
+      break;
+
+    case "get":
+      contacts.getContactById(id);
+      break;
+
+    case "add":
+      contacts.addContact(name, email, phone);
+      break;
+
+    case "remove":
+      contacts.removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
