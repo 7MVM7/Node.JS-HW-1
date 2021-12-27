a63bc422e840b759c9e030bb6b4ed529049db64a
Vladyslaw Makhnyk <itdelive@gmail.com>
2021-12-27 16:30:44 +0200

    -

--- model/Schemas/contact-schema.js
+++ model/Schemas/contact-schema.js
@@ -1,48 +1,40 @@
-const mongoose = require("mongoose");
-const { Schema, model } = mongoose;
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
-const contactSchema = new Schema(
+const contactSchema = Schema(
   {
     name: {
       type: String,
-      required: [true, "Set your name, please"],
+      required: [true, "Set name for contact"],
       minlength: 3,
-      maxlength: 30,
-      unique: true,
     },
     email: {
       type: String,
-      required: [true, "Set your email, please"],
-      unique: true,
+      required: true,
+      minlength: 6,
     },
     phone: {
       type: String,
-      required: [true, "Set your phone number, please"],
-      unique: true,
-    },
-    subscription: {
-      type: String,
-      enum: ["free", "pro", "premium"],
-      default: "free",
       required: true,
     },
-    password: {
-      type: String,
-      default: "password",
-      required: [true, "Set your password, please"],
-    },
-    token: {
-      type: String,
-      default: "",
+    favourite: {
+      type: Boolean,
+      default: false,
     },
   },
   { versionKey: false, timestamps: true }
 );
 
-contactSchema.virtual("contactId").get(function () {
-  return this._id;
+const joiSchema = Joi.object({
+  name: Joi.string().min(2).required(),
+  email: Joi.string().min(6).required(),
+  phone: Joi.string().min(7).required(),
+  favourite: Joi.bool,
 });
 
 const Contact = model("contact", contactSchema);
 
-module.exports = Contact;
+module.exports = {
+  Contact,
+  joiSchema,
+};
