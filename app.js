4c6becf8b99fe5ffe99399440fd311e5e7be0a7f
Vladyslaw Makhnyk <itdelive@gmail.com>
2021-12-27 15:45:22 +0200

    -

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const mongoose = require("mongoose");
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const DB_HOST =
-  "mongodb+srv://Vladyslav:obwYZkCfM8WZd7zc@cluster0.bsw1x.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const contactsRouter = require("./routes/api/contacts-api");
 
-mongoose
-  .connect(DB_HOST)
+const app = express();
 
-  .then(() => {
-    console.log("database connect success");
-  })
-  .catch(error.message);
-process.exit(1);
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((_req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500).json({ message: err.message });
+});
+
+module.exports = app;
--- valid/validation.js
+++ valid/validation.js
@@ -1,77 +1,49 @@
-const fs = require("fs/promises");
-const path = require("path");
-const { v4: uuid } = require("uuid");
+const Joi = require("joi");
 
-const contactsPath = path.join(__dirname, "contacts.json");
+const schemaAddContact = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  phone: Joi.string().min(10).max(13).required(),
+  subscription: Joi.string().valid("free", "pro", "premium").default("free"),
+  password: Joi.string().required(),
+  token: Joi.string().allow("").default(""),
+});
 
-async function listContacts() {
-  try {
-    const file = await fs.readFile(contactsPath, "utf-8");
-    const contacts = JSON.parse(file);
-    return contacts;
-  } catch (err) {
-    return console.error(err.message);
+const schemaUpdateContact = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).optional(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .optional(),
+  phone: Joi.string().min(10).max(13).optional(),
+  subscription: Joi.string().valid("free", "pro", "premium").default("free"),
+  password: Joi.string().optional(),
+  token: Joi.string().allow("").default(""),
+});
+
+const validate = (schema, obj, next) => {
+  const { error } = schema.validate(obj);
+  if (error) {
+    const [{ message }] = error.details;
+    return next({
+      status: 400,
+      message: `Filled: ${message.replace(/"/g, "")}`,
+    });
   }
-}
+  next();
+};
 
-async function getContactById(contactId) {
-  try {
-    const contacts = await listContacts();
-    const contactById = contacts.find(({ id }) => id.toString() === contactId);
-    return contactById;
-  } catch (err) {
-    console.error(err.message);
-  }
-}
+module.exports.addContact = (req, _res, next) => {
+  return validate(schemaAddContact, req.body, next);
+};
 
-async function removeContact(contactId) {
-  try {
-    const contacts = await listContacts();
-    const contact = contacts.find(({ id }) => id.toString() === contactId);
-    const updatedContacts = contacts.filter(
-      ({ id }) => id.toString() !== contactId
-    );
-    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, "utf8"));
-    return contact;
-  } catch (err) {
-    console.error(err.message);
-  }
-}
-
-async function addContact(body) {
-  try {
-    const contacts = await listContacts();
-    const newContact = { ...body, id: uuid() };
-    const updatedContacts = [...contacts, newContact];
-    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, "utf8"));
-    return newContact;
-  } catch (err) {
-    console.error(err.message);
-  }
-}
-
-async function updateContact(contactId, body) {
-  try {
-    const contacts = await listContacts();
-    const targetContactIndex = contacts.findIndex(
-      ({ id }) => id.toString() === contactId
-    );
-    if (targetContactIndex === -1) return;
-    contacts[targetContactIndex] = {
-      ...contacts[targetContactIndex],
-      ...body,
-    };
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, "utf8"));
-    return contacts[targetContactIndex];
-  } catch (err) {
-    console.error(err.message);
-  }
-}
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+module.exports.updateContact = (req, _res, next) => {
+  return validate(schemaUpdateContact, req.body, next);
 };
