1d9f6227e1b641463198e4c1686020ab36476dc8
Vladyslaw Makhnyk <itdelive@gmail.com>
2022-01-22 15:15:57 +0200

    Done

--- routes/api/contacts-api.js
+++ routes/api/contacts-api.js
@@ -1,32 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
-const { controllerWrapper, validation } = require("../../middlewares");
+const { ctrlWrapper, validation } = require("../../middlewares");
 const { contactsControllers } = require("../../controllers");
 const { joiSchema, updateActiveJoiSchema } = require("../../models/contacts");
 
-router.get("/", controllerWrapper(contactsControllers.getAll));
+router.get("/", ctrlWrapper(contactsControllers.getAll));
 
-router.get("/:contactId", controllerWrapper(contactsControllers.getById));
+router.get("/:contactId", ctrlWrapper(contactsControllers.getById));
 
 router.post(
   "/",
   validation(joiSchema, "missing required name field"),
-  controllerWrapper(contactsControllers.add)
+  ctrlWrapper(contactsControllers.add)
 );
 
-router.delete("/:contactId", controllerWrapper(contactsControllers.removeById));
+router.delete("/:contactId", ctrlWrapper(contactsControllers.removeById));
 
 router.put(
   "/:contactId",
   validation(joiSchema, "missing fields"),
-  controllerWrapper(contactsControllers.updateActive)
+  ctrlWrapper(contactsControllers.updateActive)
 );
 
 router.patch(
   "/:contactId",
   validation(updateActiveJoiSchema, "missing field favorite"),
-  controllerWrapper(contactsControllers.updateById)
+  ctrlWrapper(contactsControllers.updateById)
 );
 
 module.exports = router;
